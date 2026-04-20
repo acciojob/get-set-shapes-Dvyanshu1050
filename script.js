f3aca46558df7a2f72c5f691e2c7dee61f938f41
DIVYANSHU KUMAR MAURYA <155415613+Dvyanshu1050@users.noreply.github.com>
2026-04-20 22:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // getter for height
+  get height() {
+    return this._height;
+  }
+
+  // area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class (inherit)
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // width = height = side
+  }
+
+  // perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
+
+// Do not change
 window.Rectangle = Rectangle;
 window.Square = Square;
